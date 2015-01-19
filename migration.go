c289fdb1c84680a5dd2a21ed60d2d0c7d3cae9f0
bom_d_van <bom.d.van@gmail.com>
2015-01-19 09:45:31 +0800

    add migration go file (forgot to commit :P)

---
+++ migration.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"text/template"
+
+	"github.com/cheggaaa/pb"
+)
+
+func migrate(s Server, migrations []string) {
+	cmd("mkdir", "-p", "tmp/migrations")
+
+	for _, migration := range migrations {
+		base := filepath.Base(migration)
+		cmd("go", "build", "-o", "tmp/migrations/"+base, migration)
+	}
+
+	bundleMigration(migrations)
+
+	s.uploadMigration(migrations)
+
+	s.runMigration(migrations)
+}
+
+func bundleMigration(migrations []string) {
+	dst, err := os.OpenFile("tmp/migrations.tar.gz", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer dst.Close()
+	gzipw := gzip.NewWriter(dst)
+	defer gzipw.Close()
+	tarw := tar.NewWriter(gzipw)
+	defer tarw.Close()
+
+	for _, migration := range migrations {
+		base := filepath.Base(migration)
+		file, err := os.Open("tmp/migrations/" + base)
+		if err != nil {
+			exitf("failed to open tmp/migrations/%s: %s", base, err)
+		}
+		fi, err := file.Stat()
+		if err != nil {
+			exitf("failed to stat %s: %s", file.Name(), err)
+		}
+		writeToTar(tarw, "migration/"+base, file, fi)
+	}
+}
+
+func (s Server) uploadMigration(migrations []string) {
+	src, err := os.OpenFile("tmp/migrations.tar.gz", os.O_RDONLY, 0644)
+	if err != nil {
+		exitf("failed to open tmp/migrations.tar.gz: %s", err)
+	}
+	defer func() { src.Close() }()
+
+	fi, err := src.Stat()
+	if err != nil {
+		exitf("failed to retrieve file info of %s: %s", src.Name(), err)
+	}
+
+	session := s.getSession()
+	defer session.Close()
+
+	go func() {
+		dst, err := session.StdinPipe()
+		if err != nil {
+			exitf("failed to get StdinPipe: %s", err)
+		}
+		defer dst.Close()
+
+		bar := pb.New(int(fi.Size())).SetUnits(pb.U_BYTES)
+		bar.Start()
+		defer bar.Finish()
+		dstw := io.MultiWriter(bar, dst)
+
+		_, err = fmt.Fprintln(dst, "C0644", fi.Size(), "migrations.tar.gz")
+		if err != nil {
+			exitf("failed to open migrations.tar.gz: %s", err)
+		}
+		_, err = io.Copy(dstw, src)
+		if err != nil {
+			exitf("failed to upload migrations.tar.gz: %s", err)
+		}
+		_, err = fmt.Fprint(dst, "\x00")
+		if err != nil {
+			exitf("failed to close migrations.tar.gz: %s", err)
+		}
+	}()
+
+	if output, err := session.CombinedOutput("/usr/bin/scp -qrt ./"); err != nil {
+		exitf("Failed to run: %s %s", string(output), err)
+	}
+}
+
+var migrationScript = template.Must(template.New("").Parse(`set -e
+tar mxf migrations.tar.gz
+{{range .}}
+echo running {{.}}
+migration/{{.}}
+{{end}}
+`))
+
+func (s Server) runMigration(migrations []string) {
+	session := s.getSession()
+	var script bytes.Buffer
+	var bases []string
+	for _, migration := range migrations {
+		bases = append(bases, filepath.Base(migration))
+	}
+	migrationScript.Execute(&script, bases)
+
+	// TODO: refactor
+	{
+		r, err := session.StdoutPipe()
+		if err != nil {
+			exitf("failed to get stdoutPipe: %s", err)
+		}
+		go io.Copy(os.Stdout, r)
+	}
+	{
+		r, err := session.StderrPipe()
+		if err != nil {
+			exitf("failed to get StderrPipe: %s", err)
+		}
+		go io.Copy(os.Stderr, r)
+	}
+
+	err := session.Run(script.String())
+	if err != nil {
+		exitf("failed at runing script: %s\n%s", err, script)
+	}
+}
