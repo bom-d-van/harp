6ef7a92a85c7977659d7e67a1993b75d696069af
bom_d_van <bom.d.van@gmail.com>
2015-01-25 16:29:05 +0800

    update test script

--- harp_test.sh
+++ harp_test.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-docker run -P -d -v ~/.ssh/id_rsa.pub:/home/app/.ssh/authorized_keys --name server1 sshd
-docker run -P -d -v ~/.ssh/id_rsa.pub:/home/app/.ssh/authorized_keys --name server2 sshd
+set -e
 
-go run harp.go -c test/harp.json
+server1=harp_test_server1
+server2=harp_test_server2
+
+[ `docker ps -a | grep -o $server1` ] && {
+	docker rm -f $server1
+}
+
+[ `docker ps -a | grep -o $server2` ] && {
+	docker rm -f $server2
+}
+
+docker run -P -p 49153:22 -d -v ~/.ssh/id_rsa.pub:/home/app/.ssh/authorized_keys --name $server1 sshd
+docker run -P -p 49154:22 -d -v ~/.ssh/id_rsa.pub:/home/app/.ssh/authorized_keys --name $server2 sshd
+
+go build -o tmp/harp
+tmp/harp -c test/harp.json -s prod deploy
+tmp/harp -c test/harp.json -s prod -m test/migration.go,test/migration2.go migrate
