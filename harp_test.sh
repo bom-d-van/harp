a334d720f49bd5b98a3c58b9f143480d23f390c2
bom_d_van <bom.d.van@gmail.com>
2015-01-25 16:51:07 +0800

    build before set up test env

--- harp_test.sh
+++ harp_test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+go build -o tmp/harp
+
 server1=harp_test_server1
 server2=harp_test_server2
 
@@ -16,6 +18,5 @@ server2=harp_test_server2
 docker run -P -p 49153:22 -d -v ~/.ssh/id_rsa.pub:/home/app/.ssh/authorized_keys --name $server1 sshd
 docker run -P -p 49154:22 -d -v ~/.ssh/id_rsa.pub:/home/app/.ssh/authorized_keys --name $server2 sshd
 
-go build -o tmp/harp
 tmp/harp -c test/harp.json -s prod deploy
 tmp/harp -c test/harp.json -s prod -m test/migration.go,test/migration2.go migrate
